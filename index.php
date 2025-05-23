e84c07738034a47139809f7ab11740db26fccdeb
Marvin Lamart <m.lamart@hotmail.fr>
2025-05-23 16:22:22 +0200

    feat(test-inte): integrate strip-map

--- index.php
+++ index.php
@@ -7,10 +7,42 @@ $twig = new \Twig\Environment($loader, [
 ]);
 $twig->addExtension(new \Twig\Extension\DebugExtension());
 
+function hasChildWithTerminus($station) {
+    if (!isset($station['children']) || empty($station['children'])) {
+        return false;
+    }
+
+    foreach ($station['children'] as $child) {
+        if ($child['type'] === 'terminus') {
+            return true;
+        }
+
+        // Recursively check for children of the child
+        if (isset($child['children']) && !empty($child['children'])) {
+            if (hasChildWithTerminus($child)) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+$twig->addFunction(new \Twig\TwigFunction('hasChildWithTerminus', 'hasChildWithTerminus'));
+
+$twig->addFunction(new \Twig\TwigFunction('isCurrentStation', function ($station, $currentStation) {
+    return isset($station['name']) && $station['name'] === $currentStation;
+}));
+
+$twig->addFunction(new \Twig\TwigFunction('isTerminus', function ($station) {
+    return isset($station['type']) && $station['type'] === 'terminus';
+}));
+
+
 
 // Render the template
 echo $twig->render('index.html', [
-    'title' => 'Horaires',
+    'title' => 'Test d’intégration',
     'schedule_title' => 'LUNDI AU VENDREDI - Monday to Friday',
     'schedule' => [
         5 => [],
@@ -32,4 +64,54 @@ echo $twig->render('index.html', [
         21 => [],
         22 => [],
     ],
+    'line_data' => [
+        'name' => 'M3',
+        'color' => '#70007c',
+        'current_station' => 'Olympia',
+        'stations' => [
+            ['name' => 'PISCINE YVES BLANC', 'type' => 'terminus'],
+            ['name' => 'Cimetière St Pierre', 'type' => 'normal'],
+            ['name' => 'Parking Carcassonne', 'type' => 'normal'],
+            ['name' => 'Tour D\'Aygosi', 'type' => 'normal'],
+            ['name' => 'Les Cigales', 'type' => 'normal'],
+            ['name' => 'Bourget', 'type' => 'normal'],
+            ['name' => 'Saint Pierre', 'type' => 'normal'],
+            ['name' => 'Gambetta', 'type' => 'normal'],
+            ['name' => 'Ganay', 'type' => 'normal'],
+            ['name' => 'Bellegarde', 'type' => 'normal'],
+            ['name' => 'Violette', 'type' => 'normal'],
+            ['name' => 'Gianotti', 'type' => 'normal'],
+            ['name' => 'Pasteur', 'type' => 'normal'],
+            ['name' => 'Pasteur Notre-Dame', 'type' => 'normal'],
+            ['name' => 'Solari', 'type' => 'normal'],
+            ['name' => 'Nation', 'type' => 'normal'],
+            ['name' => 'Hôpital', 'type' => 'normal'],
+            ['name' => 'Hôpital Tamaris', 'type' => 'normal'],
+            ['name' => 'Rocher Du Dragon', 'type' => 'normal'],
+            ['name' => 'Parc Mozart', 'type' => 'normal'],
+            [
+                'name' => 'Pontier',
+                'type' => 'normal',
+                'children' => [
+                    ['name' => 'Renoir', 'type' => 'normal'],
+                    ['name' => 'Olympia', 'type' => 'normal'],
+                    [
+                        'name' => 'Fleury-Val', 
+                        'type' => 'normal',
+                        'children' => [
+                            ['name' => 'Chalet', 'type' => 'normal'],
+                            ['name' => 'Koenig', 'type' => 'normal'],
+                            ['name' => 'Seigneurie', 'type' => 'normal']
+                        ]
+                    ],
+                    ['name' => 'Lauriers', 'type' => 'normal'],
+                    ['name' => 'Boutière', 'type' => 'normal'],
+                    ['name' => 'BEAUVALLON', 'type' => 'terminus'],
+                ]
+            ],
+            ['name' => 'Guyon', 'type' => 'normal'],
+            ['name' => 'Verte Colline', 'type' => 'normal'],
+            ['name' => 'LA CHEVALIERE', 'type' => 'terminus']
+        ]
+    ]
 ]);
